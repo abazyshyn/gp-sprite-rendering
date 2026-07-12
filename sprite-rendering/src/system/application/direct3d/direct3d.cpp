46102d93c0cf3b45bcd037b7e907c76674291964
abazyshyn <maybebazishin@gmail.com>
2026-07-12 10:09:01 +0300

    feat: direct3d class

--- sprite-rendering/src/system/application/direct3d/direct3d.cpp
+++ sprite-rendering/src/system/application/direct3d/direct3d.cpp
@@ -96,6 +96,89 @@ namespace GP
         factory->Release();
         factory = nullptr;
 
+        DXGI_SWAP_CHAIN_DESC swapChainDescription{};
+        swapChainDescription.BufferCount = 1;
+        swapChainDescription.BufferDesc.Width = windowWidth;
+        swapChainDescription.BufferDesc.Height = windowHeight;
+        swapChainDescription.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+        swapChainDescription.BufferDesc.RefreshRate.Numerator = m_vsync ? refreshRateNumenator : 0;
+        swapChainDescription.BufferDesc.RefreshRate.Denominator = m_vsync ? refreshRateDenominator : 0;
+        swapChainDescription.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+        swapChainDescription.OutputWindow = hWnd;
+        swapChainDescription.SampleDesc.Count = 1;   // Turn multisampling off
+        swapChainDescription.SampleDesc.Quality = 0; //
+        swapChainDescription.Windowed = !fullscreen;
+        swapChainDescription.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+        swapChainDescription.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+        swapChainDescription.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+        swapChainDescription.Flags = 0;
+
+        D3D_FEATURE_LEVEL featureLevel = D3D_FEATURE_LEVEL_11_0;
+
+        if (FAILED(D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0, &featureLevel, 1, D3D11_SDK_VERSION,
+                                                 &swapChainDescription, &m_swapChain, &m_device, nullptr, &m_deviceContext)))
+        {
+            MessageBox(hWnd, L"Could not create IDXGISwapChain", L"Error", MB_OK);
+            return false;
+        }
+
+        ID3D11Texture2D *backBuffer = nullptr;
+        if (FAILED(m_swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void **>(&backBuffer))))
+        {
+            MessageBox(hWnd, L"Could not retrieve ID3D11Texture2D backBuffer from swap chain", L"Error", MB_OK);
+            return false;
+        }
+
+        if (FAILED(m_device->CreateRenderTargetView(backBuffer, nullptr, &m_renderTargetView)))
+        {
+            MessageBox(hWnd, L"Could not create ID3D11RenderTargetView", L"Error", MB_OK);
+            return false;
+        }
+
+        backBuffer->Release();
+        backBuffer = nullptr;
+
+        D3D11_TEXTURE2D_DESC depthStencilBufferDescription{};
+        depthStencilBufferDescription.Width = static_cast<uint32_t>(windowWidth);
+        depthStencilBufferDescription.Height = static_cast<uint32_t>(windowHeight);
+        depthStencilBufferDescription.MipLevels = 1;
+        depthStencilBufferDescription.ArraySize = 1;
+        depthStencilBufferDescription.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+        depthStencilBufferDescription.SampleDesc.Count = 1;
+        depthStencilBufferDescription.SampleDesc.Quality = 0;
+        depthStencilBufferDescription.Usage = D3D11_USAGE_DEFAULT;
+        depthStencilBufferDescription.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+        depthStencilBufferDescription.CPUAccessFlags = 0;
+        depthStencilBufferDescription.MiscFlags = 0;
+
+        if (FAILED(m_device->CreateTexture2D(&depthStencilBufferDescription, nullptr, &m_depthStencilBuffer)))
+        {
+            MessageBox(hWnd, L"Could not create ID3D11Texture2D m_depthStencilBuffer", L"Error", MB_OK);
+            return false;
+        }
+
+        D3D11_DEPTH_STENCIL_DESC depthStencilDescription{};
+        depthStencilDescription.DepthEnable = true;
+        depthStencilDescription.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
+        depthStencilDescription.DepthFunc = D3D11_COMPARISON_LESS;
+        depthStencilDescription.StencilEnable = true;
+        depthStencilDescription.StencilReadMask = 0xFF;
+        depthStencilDescription.StencilWriteMask = 0xFF;
+        depthStencilDescription.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+        depthStencilDescription.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_INCR;
+        depthStencilDescription.FrontFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+        depthStencilDescription.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+        depthStencilDescription.BackFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+        depthStencilDescription.BackFace.StencilDepthFailOp = D3D11_STENCIL_OP_DECR;
+        depthStencilDescription.BackFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+        depthStencilDescription.BackFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+
+        if (FAILED(m_device->CreateDepthStencilState(&depthStencilDescription, &m_depthDisabledStencilState)))
+        {
+            MessageBox(hWnd, L"Could not create ID3D11DepthStencilState m_depthDisabledStencilState", L"Error", MB_OK);
+            return false;
+        }
+
         return true;
     }
 
